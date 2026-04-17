f47db2868412a3f5e00b36e520b7d3d0d5a1c794
Hatim Oudghiri <oudhatim07@gmail.com>
2026-04-17 10:22:56 -0400

    Update API base URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
